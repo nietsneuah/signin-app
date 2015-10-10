a87a43bc2e976b877ba95df6a0b38d666026ab7e
Doug <doug.hauenstein@gmail.com>
2015-10-10 14:50:02 -0400

    Add checkbox and delete function

--- signin.jsx
+++ signin.jsx
@@ -5,10 +5,39 @@ Signin = React.createClass({
     // We can use propTypes to indicate it is required
     signin: React.PropTypes.object.isRequired
   },
+  toggleChecked() {
+    // Set the toggled property to the opposite value
+    Signins.update(this.props.signin._id, {
+      $set: {checked: ! this.props.signin.checked}
+
+    });
+  },
+
+  deleteThisTask() {
+    Signins.remove(this.props.signin._id);
+  },
+
   render() {
+    // Gives signins a different className when they are checked off
+    // so they can be styled nicely with CSS
+    const taskClassName = this.props.signin.checked ? "checked" : "";
+
     return (
         // must be in an HTML tag
-        <li>{this.props.signin.guest} is a guest of {this.props.signin.member}</li>
+      <li className={taskClassName}>
+        <button className="delete" onClick={this.deleteThisTask}>
+          &times;
+        </button>
+
+        <input
+          type="checkbox"
+          readOnly={true}
+          checked={this.props.signin.checked}
+          onClick={this.toggleChecked}/>
+
+        <span className="text">  {this.props.signin.guest} is a guest of {this.props.signin.member}</span>
+
+        </li>
 
     );
   }
